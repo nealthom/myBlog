0be23f93c6a8102432b1e19fb4095367e7261337
nealthom <nealthom@gmail.com>
2020-01-13 23:36:07 -0600

    getting started with about and contact

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+export default () => (
+  <Layout>
+    <div>
+      <h1>About me</h1>
+      <p>I'm doing stuff and it's fun</p>
+    </div>
+  </Layout>
+)
