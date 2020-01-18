6264e9a411973a62e3c6e559ae9902de0ae842a8
nealthom <nealthom@gmail.com>
2020-01-18 02:06:53 -0600

    manifest

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,6 +22,7 @@ export default ({ children }) => {
         max-width: 700px;
         padding: ${rhythm(2)};
         padding-top: ${rhythm(1.5)};
+        border: black solid 2px;
       `}
     >
       <Link to={`/`}>
