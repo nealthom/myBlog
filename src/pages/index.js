f9579a7a9785131fa929191c6fbd6f930958de96
nealthom <nealthom@gmail.com>
2020-01-19 04:27:50 -0600

    posts from wordpress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import { css } from "@emotion/core"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
@@ -7,8 +8,15 @@ export default ({ data }) => {
   return (
     <Layout>
       <SEO title="home" />
-      <h1>Thinking About Things</h1>
-      <h4>Posts</h4>
+      <h1
+        css={css`
+          display: inline-block;
+          border-bottom: 1px solid;
+        `}
+      >
+        Thinking About Things
+      </h1>
+
       {data.allWordpressPost.edges.map(({ node }) => (
         <div key={node.slug}>
           <Link to={node.slug}>
@@ -23,12 +31,13 @@ export default ({ data }) => {
 
 export const pageQuery = graphql`
   query {
-    allWordpressPost(sort: { fields: [date] }) {
+    allWordpressPost(sort: { fields: [date], order: DESC }) {
       edges {
         node {
           title
           excerpt
           slug
+          date
         }
       }
     }
