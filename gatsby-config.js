014fe5c59c75f14c01bc495513d065ee30fe7e14
nealthom <nealthom@gmail.com>
2020-01-21 23:46:44 -0600

    update manifest styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,11 +23,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `GatsbyJS`,
-        short_name: `GatsbyJS`,
+        name: `Thomas Neal`,
+        short_name: `Nealthom`,
         start_url: `/`,
-        background_color: `#6b37bf`,
-        theme_color: `#6b37bf`,
+        background_color: `#0ccac4`,
+        theme_color: `#0ccac4`,
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
